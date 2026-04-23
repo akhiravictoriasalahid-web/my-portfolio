6fd95bea4c88d3723ff61dae405238f05452563f
akhiravictoriasalahid-web <akhiravictoriasalahid@gmail.com>
2026-04-23 13:34:00 +0200

    Add routing and Navbar navigation

--- src/App.jsx
+++ src/App.jsx
@@ -1,19 +1,23 @@
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 
-import Home from "./Pages/Home";
-import About from "./Pages/About";
-import Portfolio from "./Pages/Portfolio";
+import Home from "./Pages/Home.jsx";
+import About from "./Pages/About.jsx";
+import Portfolio from "./Pages/Portfolio.jsx";
+import Navbar from "./Navbar.jsx";
 
 function App() {
   return (
+    <>
     <BrowserRouter>
-     <Routes>
-      <Route path="/" element={<Home />} />
-      <Route path="/about" element={<About />} />
-      <Route path="/portfolio" element={<Portfolio />} />
-     </Routes>
+     <Navbar></Navbar>
+      <Routes>
+       <Route path="/" element={<Home />} />
+       <Route path="/about" element={<About />} />
+       <Route path="/portfolio" element={<Portfolio />} />
+      </Routes>
     </BrowserRouter>
-  );
+    </>
+  )
 
 }
 
